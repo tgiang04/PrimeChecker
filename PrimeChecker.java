35a31b6104eed94e267790e285079f2eeebcd0a5
tgiang04 <119372224+tgiang04@users.noreply.github.com>
2024-04-19 04:27:36 +0000

    PrimeChecker lỗi không code

--- PrimeChecker.java
+++ PrimeChecker.java
@@ -1,1 +1,28 @@
+import java.util.Scanner;
+
+public class PrimeChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập một số nguyên dương: ");
+        int number = scanner.nextInt();
+
+        if(isPrime(number)) {
+            System.out.println(number + " là số nguyên tố.");
+        } else {
+            System.out.println(number + " không là số nguyên tố.");
+        }
+    }
+
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
 
